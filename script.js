b78e06c637af8fe390b7a33a15777fd00cff838b
Bigounce016 <elicreynolds928@gmail.com>
2026-03-06 08:35:49 -0800

    Worked on JS and CSS

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-async function data() {
-    const url = "https://eecu-data-server.vercel.app/data";
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error("Error fetching data:", error);
+let choice = {
+    Occupation: "",
+    Salary: 0
+};
+
+
+async function getCareers() {
+        const url = "https://eecu-data-server.vercel.app/data";
+        try {
+            const response = await fetch(url);
+            const jobs = await response.json();
+            createOptions(jobs);
+            return jobs;
+        }
+        catch (error) {
+            console.error("Error fetching careers data:", error);
+            return [];
+        }
     }
 
+    function createOptions(careers) {
+        const list = document.getElementById("careerList");
 
-}
+        careers.forEach((career, index) => {
+        const option = document.createElement("section");
+
+         option.innerHTML = `${career.Occupation}: $${career.Salary}`;
+         option.setAttribute("id", `${index}`);
+         option.addEventListener("click", () => {
+            choice.Occupation = career.Occupation;
+            choice.Salary = career.Salary;
+            console.log(choice);
+         });
+         option.onclick = function() {
+            window.location.href = "Budget.html";
+         }
+         option.classList.add("option");
+
+        list.appendChild(option);
+        });
+
+        
+    }
+    getCareers();
+
